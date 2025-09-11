bb7a9e2b44bbb2a2286339f6c363fd9833307856
bkrsinclairrr <137005674+bkrsinclairrr@users.noreply.github.com>
2025-09-11 17:08:14 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import { useState, useEffect } from "react"
 import Login from "./Login"
 import Home from "./Home"
 
-export default function App() {
+function App() {
   const [autenticado, setAutenticado] = useState(false)
 
   useEffect(() => {
-    if (localStorage.getItem("autenticado") === "true") {
+    // Se já fez login antes, mantém autenticado
+    const auth = localStorage.getItem("autenticado")
+    if (auth === "true") {
       setAutenticado(true)
     }
   }, [])
 
-  return autenticado ? (
-    <Home />
-  ) : (
-    <Login onLogin={() => setAutenticado(true)} />
+  return (
+    <>
+      {autenticado ? (
+        <Home />
+      ) : (
+        <Login onLogin={() => setAutenticado(true)} />
+      )}
+    </>
   )
 }
+
+export default App
